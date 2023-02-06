dd9a7db3bacf3e35ea425afd025794c004396df1
Truimo <xiaomo@20mo.cn>
2023-02-06 22:24:27 +0800

    fix: images remote hostname push

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 's3.us-west-2.amazonaws.com',
+        }, {
+            protocol: 'https',
+            hostname: 'xxgame-oss-1305181693.file.myqcloud.com',
+        }, {
+            protocol: 'https',
+            hostname: 'shp.qpic.cn',
+        }, {
+            protocol: 'https',
+            hostname: 'img.truimo.com',
+        }]
+    }
+}
