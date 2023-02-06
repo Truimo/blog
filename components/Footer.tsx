9a6b761e2bbc77d788d74968d63491b537358eb9
Truimo <xiaomo@20mo.cn>
2023-02-06 22:22:16 +0800

    feat: footer add theme switch, and add more content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,26 @@
-function Footer() {
-    return (
-        <footer className="relative bg-gray-100 dark:bg-gray-800">
-            <div className="flex flex-col">
-                <nav className="">
+import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
 
-                </nav>
-                <section className="pt-2 pb-5 text-gray-400 dark:text-gray-500">
-                    footer
-                </section>
+function Footer() {
+    const thisYear = new Date().getFullYear()
+    return (
+        <footer className="relative text-xs text-neutral-500 dark:text-neutral-600 bg-neutral-100 dark:bg-neutral-800">
+            <div className="px-6 pt-3 pb-5">
+                <div className="flex flex-col">
+                    <div className="flex justify-end">
+                        <ThemeSwitch/>
+                    </div>
+                    <section className="leading-5">
+                        <p>Powered by <a href="https://nextjs.org/" className="hover:underline" target="_blank" rel="nofollow noreferrer">NextJS</a>, <a href="https://tailwindcss.com/" className="hover:underline" target="_blank" rel="nofollow noreferrer">Tailwindcss</a>, <a href="https://developers.notion.com/" className="hover:underline" target="_blank" rel="nofollow noreferrer">Notion</a></p>
+                        <p>Design & Build with 💖 by <a href="https://github.com/truimo/blog" className="hover:underline" target="_blank" rel="nofollow noreferrer">Truimo</a></p>
+                        <div className="my-1 h-px bg-gray-200 dark:bg-gray-700"></div>
+                        <div className="flex justify-between flex-wrap">
+                            <p>Copyright &copy; 2019-{thisYear} <Link href="/">浅小沫的博客</Link></p>
+                            <p><a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.zh" className="hover:underline" target="_blank" rel="nofollow noreferrer">CC BY-NC-ND 4.0</a></p>
+                        </div>
+                        <p><a className="hover:underline" href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">湘ICP备2020021033号-1</a></p>
+                    </section>
+                </div>
             </div>
         </footer>
     )
