18a8d468fadc046d4791b42eea7183057c26b15b
Truimo <xiaomo@20mo.cn>
2023-02-08 13:47:07 +0800

    fix: remove umami script from _app to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ import type {AppProps} from "next/app";
 import {ThemeProvider} from "next-themes";
 import Head from "next/head";
 import BlogLayout from '../components/layout/BlogLayout';
-import Script from "next/script";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -28,8 +27,6 @@ function App({Component, pageProps}: AppPropsWithLayout) {
                       content="width=device-width,initial-scale=1.0,maximum-scale=1.0,viewport-fit=cover,user-scalable=0"/>
                 <title>浅小沫的博客</title>
                 <link rel="shortcut icon" type="image/png" href="https://q1.qlogo.cn/g?b=qq&nk=3201719830&s=640"/>
-                <Script src="https://analyze.truimo.com/umami.js" async defer
-                        data-website-id="e0b642ae-836d-4cf7-b1d7-30b8be34608a" data-domains="blog.truimo.com"/>
             </Head>
             <ThemeProvider attribute="class">
                 {getLayout(
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import {Head, Html, Main, NextScript} from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
         <Html lang="zh-CN">
-            <Head/>
+            <Head>
+                <Script src="https://analyze.truimo.com/umami.js" strategy="lazyOnload"
+                        data-website-id="e0b642ae-836d-4cf7-b1d7-30b8be34608a" data-domains="blog.truimo.com"/>
+            </Head>
             <body>
             <Main/>
             <NextScript/>
