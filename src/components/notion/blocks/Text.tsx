72a2eaeb516c1ca0c424895b1c56e59ee7fc9b77
Truimo <xiaomo@20mo.cn>
2023-11-27 00:15:41 +0800

    feat: link

--- src/components/notion/blocks/Text.tsx
+++ src/components/notion/blocks/Text.tsx
@@ -1,3 +1,4 @@
+import {clsxm} from '@/libs/helper'
 import {getAnnotationsClass} from '@/libs/notion'
 import type {TextRichTextItemResponse} from '@notionhq/client/build/src/api-endpoints'
 
@@ -6,8 +7,8 @@ export default function Text({text}: {
 }) {
     if (text.text.link) {
         return (
-            <a className={getAnnotationsClass(text.annotations)} href={text.text.link.url}
-               target="_blank">{text.text.content}</a>
+            <a className={clsxm('underline', getAnnotationsClass(text.annotations))} href={text.text.link.url}
+               rel="noreferrer" target="_blank">{text.text.content}<i className="icon-[mingcute--external-link-line] translate-y-[2px]"></i></a>
         )
     }
 
