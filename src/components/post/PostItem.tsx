b6996f9ed295f496c555b92323333804f87f72c5
Truimo <xiaomo@20mo.cn>
2024-08-26 11:36:49 +0800

    feat: post list style

--- src/components/post/PostItem.tsx
+++ src/components/post/PostItem.tsx
@@ -7,15 +7,15 @@ export default function PostItem({post}: {
     post: PostMeta
 }) {
     return (
-        <article className="my-3">
+        <article className="my-8">
             <Link href={`/posts/${post.slug}`}>
                 <p className="text-sm leading-normal">
                     <Category category={post.category}/>
                     <span className="font-bold">&nbsp;·&nbsp;</span>
                     <time dateTime={post.date}>{formatDate(post.date, 'YYYY 年 MM 月 DD 日')}</time>
                 </p>
-                <h2 className="text-xl font-bold leading-relaxed">{post.title}</h2>
-                <p className="leading-normal">{post.excerpt}</p>
+                <h2 className="text-xl font-medium my-2">{post.title}</h2>
+                <p className="leading-loose text-gray-800/90">{post.excerpt}</p>
             </Link>
         </article>
     )
