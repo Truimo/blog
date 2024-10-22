97bd9cd5e324a5b963955c676875229e141f9a75
Truimo <xiaomo@20mo.cn>
2024-10-22 13:35:17 +0800

    fix: bookmark

--- src/app/api/bookmark/route.ts
+++ src/app/api/bookmark/route.ts
@@ -11,11 +11,15 @@ export async function POST(request: NextRequest) {
         return Response.json({error: 'No URL provided'}, {status: 400})
     }
 
-    const result = await unfurl(body.url, {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
-        }
-    })
+    try {
+        const result = await unfurl(body.url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+            }
+        })
 
-    return Response.json(result)
+        return Response.json(result)
+    } catch (e) {
+        return Response.json({error: 'Illegal request'}, {status: 400})
+    }
 }
