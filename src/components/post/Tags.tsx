6214b6bf7134ba439094b234e6cd19a0ffbc4fc2
Truimo <Xiaomo@20mo.cn>
2025-03-26 21:50:48 +0800

    fix: tags

--- src/components/post/Tags.tsx
+++ src/components/post/Tags.tsx
@@ -14,6 +14,12 @@ export default function Tags({className, tags}: {
     className?: string,
     tags: Tag[]
 }) {
+    if (tags.length === 0) {
+        return (
+            <span className={clsx(tagTextStyle, colorStyled('default'), className)}>无标签</span>
+        )
+    }
+
     return tags.map((tag) => {
         return (
             <span className={clsx(tagTextStyle, colorStyled(tag.color), className)} key={tag.name}>{tag.name}</span>
