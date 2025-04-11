ec69e922a3b609914faf16e9ffe09d3123d5b50e
Truimo <xiaomo@20mo.cn>
2025-04-11 11:08:03 +0800

    fix: footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import {icp} from '@/config'
-import {Container} from "@/components/layout/Container";
+import {Container} from '@/components/layout/Container'
 
 export const Footer: Component = () => {
     return (
         <footer>
             <Container className="h-16 flex justify-center items-center">
-                <p className="text-center text-sm text-zinc-500">Copyright&nbsp;&copy;&nbsp;2019 - 2025&nbsp;<Link href="/">Truimo</Link>.&nbsp;<a href="#">Blog</a>.&nbsp;
+                <p className="text-center text-sm text-zinc-500">Copyright&nbsp;&copy;&nbsp;2019 - 2025&nbsp;<Link href="/">Truimo's Blog</Link>.&nbsp;
                     {icp && (<a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">{icp}</a>)}</p>
             </Container>
         </footer>
