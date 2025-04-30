94842c053bf9c3d502aa10a6a86003a873391c8b
Truimo <xiaomo@20mo.cn>
2025-04-30 19:37:45 +0800

    fix: dayjs

--- src/libs/time.ts
+++ src/libs/time.ts
@@ -1,6 +1,6 @@
-import moment from 'moment'
+import dayjs from 'dayjs'
 
 export const formatDate = (dateStr: string, format: string = 'YYYY-MM-DD'): string => {
-    const date = moment(dateStr)
+    const date = dayjs(dateStr)
     return date.format(format)
 }
