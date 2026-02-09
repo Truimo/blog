fa9e329254b0ee2dbe13050f643a2170c1600cce
Xiaomo <Xiaomo@20mo.cn>
2026-02-09 21:40:20 +0800

    fix: 2026 year

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: Component = () => {
     return (
         <footer>
             <Container className="h-16 flex justify-center items-center">
-                <p className="text-center text-sm text-zinc-500">Copyright&nbsp;&copy;&nbsp;2019&nbsp;-&nbsp;2025&nbsp;<Link href="/">Truimo&apos;s Blog</Link>.&nbsp;
+                <p className="text-center text-sm text-zinc-500">Copyright&nbsp;&copy;&nbsp;2019&nbsp;-&nbsp;2026&nbsp;<Link href="/">Truimo&apos;s Blog</Link>.&nbsp;
                     {icp && (<a href="http://beian.miit.gov.cn/" rel="nofollow noreferrer" target="_blank">{icp}</a>)}</p>
             </Container>
         </footer>
